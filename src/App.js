611f7743f76d3e2189f657f2633f59a3d0382c46
Rej Averion <averionrej@gmail.com>
2023-03-24 16:30:19 +0400

    updated pdf generation

--- src/App.js
+++ src/App.js
@@ -203,12 +203,38 @@ const App = () => {
   const generatePDF = (inputData) => {
     const doc = new jsPDF();
 
-    // Add content to the PDF document using the input data
+    // Load the image file
+    const imgData = require("./MFTS_logo.png");
+    // Add the image to the PDF document
+    doc.addImage(
+      imgData,
+      "PNG",
+      50, // X coordinate of the image
+      10, // Y coordinate of the image
+      96, // Width of the image
+      30 // Height of the image
+    );
 
-    doc.text(`To: ${inputData.To}`, 10, 10);
-    doc.text(`Address: ${inputData.Address}`, 10, 17);
-    doc.text(`Date: ${inputData.Date}`, 100, 10);
-    doc.text(`Quotation/Invoice#: ${inputData.DocNum}`, 100, 17);
+    // Set the font and font size
+    doc.setFont("helvetica");
+    doc.setFontSize(11);
+
+    // Add content to the PDF document using the input data
+    doc.text(`To: ${inputData.To}`, 10, 55);
+    doc.text(`Address: ${inputData.Address}`, 10, 60);
+    doc.text(`Date: ${inputData.Date}`, 150, 55);
+    doc.text(`Quote/Invoice#: ${inputData.DocNum}`, 150, 60);
+
+    doc.setFontSize(14);
+    if (selectedOption === "ACKRECEIPT") {
+      doc.text(`ACKNOWLEDGEMENT RECEIPT`, 70, 75);
+    } else if (selectedOption === "INV") {
+      doc.text(`INVOICE`, 95, 75);
+    } else if (selectedOption === "QUOTE") {
+      doc.text(`QUOTATION`, 90, 75);
+    } else if (selectedOption === "SOA") {
+      doc.text(`STATEMENT OF ACCOUNT`, 70, 75);
+    }
     // ...
 
     // Save the PDF document
